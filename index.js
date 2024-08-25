9ba23f6466ee3a10727991b9d7059cbdb3519896
AI-Ashik <arifulislamashik07@gmail.com>
2024-08-25 22:18:27 +0600

    fixed error of pretttier

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const http = require("http");
-
-const port = 3000;
-const hostName = "127.0.0.1";
-
-const myServer = http.createServer((req, res) => {
-  res.end("Hello i am your first server");
-});
-
-myServer.listen(port, hostName, () => {
-  console.log(`Server is running succesfully at http://${hostName}:${port}`);
-});
+const fs = requuire('fs')
